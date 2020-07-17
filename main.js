16af4233bbe7392544d0763628ac13602b1d112c
WalaNour <nourwala548@gmail.com>
2020-07-17 08:56:36 +0200

    not working

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
- var count =0
- $('.mainSwimming').hide();
-function showSwim() {
-$('.buttons').on("click" ,function() {
+var count=0;
+var count1=0;
+      $('.mainAmer').hide();
+      $('.mainVolleyball').hide();
+      $('.mainSwimming').hide();
+function showSwim(){
 	count++;
-	if(count %2 !== 0){
-	$('.mainSwimming').show();
-   }
-   else if(count%2 ===0){
-   	$('.mainSwimming').hide();
-   }
-})
+	if(count%2 !==0){
+		$('.mainSwimming').show();
+	}
+	else if (count%2===0){
+		$('.mainSwimming').hide()
+	}
 }
+
+function showVoll(){
+	count++;
+	if(count%2 ===0){
+		$('.mainSwimming').hide()
+		$('.mainVolleyball').show();
+	}
+	else if (count%2 !==0){
+		$('.mainVolleyball').hide()
+	}
+}
+function showAmer(){
+	count++;
+	if(count%2 !== 0){
+		$('.mainVolleyball').hide()
+		$('.mainAmer').show()
+	}
+	else if(count%2 === 0){
+		$('.mainAmer').hide()
+	}
+}
+
+// function press(){
+// 	if(type(storage)!== "undefined"){
+// 		if(localstorage.press){
+// 			localstorage.press=number(localstorage.press)+1;
+// 	 }else {
+// 	 	localStorage.press=1;
+// 	 }
+// 	 document.getElementById("cunt").innerHTML='number of items'+localStorage.press;
+// 	}
+// }
+
+	var button =document.getElementById("press"),counter=0;
+ var x = button.onclick=function(){
+	counter ++
+	button.innerHTML=+ counter;
+}
+	
